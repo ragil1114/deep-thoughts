28d24daea7196e7883f611075a42507146ec5f58
Ragil14 <rahil.gilani@gmail.com>
2022-04-07 13:56:34 -0500

    clean code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,16 +5,16 @@ const resolvers = {
         // get all users
         users: async () => {
             return User.find()
-            .select('-__v -password')
-            .populate('friends')
-            .populate('thoughts');
+              .select('-__v -password')
+              .populate('friends')
+              .populate('thoughts');
         },
         // get a user by username
         user: async (parent, { username }) => {
             return User.findOne({ username })
-            .select('-__v -password')
-            .populate('friends')
-            .populate('thoughts');
+              .select('-__v -password')
+              .populate('friends')
+              .populate('thoughts');
         },
         // get all thoughts for a user
         thoughts: async (parent, { username }) => {
