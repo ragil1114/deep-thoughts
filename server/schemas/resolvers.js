868761534441db5a74977540fdba4860b33ad086
Ragil14 <rahil.gilani@gmail.com>
2022-04-07 13:55:58 -0500

    add user & users resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,13 +2,29 @@ const { User, Thought } = require('../models');
 
 const resolvers = {
     Query: {
+        // get all users
+        users: async () => {
+            return User.find()
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
+        },
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
+        },
+        // get all thoughts for a user
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
         },
+        // get a thought by id
         thought: async (parent, { _id }) => {
             return Thought.findOne({ _id });
-        },
+        }
     }
 };
   
