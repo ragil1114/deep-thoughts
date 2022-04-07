ddfbefc6ab9cb407e5a6fa6c4847a82739f1d676
Ragil14 <rahil.gilani@gmail.com>
2022-04-07 13:53:45 -0500

    add thought resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,6 +6,9 @@ const resolvers = {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
         },
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
     }
 };
   
